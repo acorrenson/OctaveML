2fa4ca49273830c1f90564fe1f518201411bfe70
Arthur Correnson <arthur.correnson@gmail.com>
2018-08-05 18:05:11 +0200

    plot hypothesis (sphere)

--- Logistic_Regression/isInSphere.m
+++ Logistic_Regression/isInSphere.m
@@ -63,13 +63,48 @@ u = class2(:, 1)';
 v = class2(:, 2)';
 w = class2(:, 3)';
 plot3(u, v, w, 'ob');
+legend('out of the sphere', 'in the sphere');
+hold on;
 
+l = 20;
 
+u = linspace(-5, 5, l);
+v = linspace(-5, 5, l);
+w = linspace(-5, 5, l);
 
+score = zeros(l .^ 3, 4);
+index = 0;
 
+for i = 1:l
+  for j = 1:l
+    for k = 1:l
+      index = index + 1;
+      a = t(1);
+      b = t(2);
+      c = t(3);
+      d = t(4);
+      e = t(5);
+      f = t(6);
+      g = t(7);
+      px = u(i);
+      py = v(j);
+      pz = w(k);
+      r = a + b*px + c*py + d*pz + e*px.^2 + f*py.^2 + g*pz.^2;
+      score(index, 4) = r;
+      score(index, 1) = u(i);
+      score(index, 2) = v(j);
+      score(index, 3) = w(k); 
+    end
+  end
+end
 
+finalScore = find(ceil(score(:,4)) == 0);
 
+finalX = score(finalScore, 1);
+finalY = score(finalScore, 2);
+finalZ = score(finalScore, 3);
 
+plot3(finalX, finalY, finalZ, '.k', 'MarkerSize', 2);
 
-
+%axis([-5 5 -5 5 -5 5]);
 
